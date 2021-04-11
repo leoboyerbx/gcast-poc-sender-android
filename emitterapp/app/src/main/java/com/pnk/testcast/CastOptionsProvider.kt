f78619820c7690550b555c93a1ceaa842321752f
leoboyerbx <contact@leoboyer.fr>
2021-04-11 23:46:03 +0200

    initial bis

--- emitterapp/app/src/main/java/com/pnk/testcast/CastOptionsProvider.kt
+++ emitterapp/app/src/main/java/com/pnk/testcast/CastOptionsProvider.kt
@@ -5,10 +5,15 @@ import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
 import com.google.android.gms.cast.framework.SessionProvider
 
-class CastOptionsProvider: OptionsProvider {
+class CastOptionsProvider : OptionsProvider {
+    val CUSTOM_NAMESPACE = "urn:x-cast:custom_namespace"
+
     override fun getCastOptions(p0: Context?): CastOptions {
+        val supportedNamespaces: MutableList<String> = ArrayList()
+        supportedNamespaces.add(CUSTOM_NAMESPACE)
         return CastOptions.Builder()
             .setReceiverApplicationId("720A8D7E")
+            .setSupportedNamespaces(supportedNamespaces)
             .build()
     }
 
--- emitterapp/app/src/main/java/com/pnk/testcast/MainActivity.kt
+++ emitterapp/app/src/main/java/com/pnk/testcast/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.pnk.testcast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.fragment.app.FragmentActivity
+import androidx.mediarouter.app.MediaRouteButton
+import com.google.android.gms.cast.framework.CastButtonFactory
+import com.google.android.gms.cast.framework.CastContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+    private lateinit var mCastContext: CastContext
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mMediaRouteButton = findViewById<MediaRouteButton>(R.id.media_route_button)
+        CastButtonFactory.setUpMediaRouteButton(applicationContext, mMediaRouteButton)
+
+        mCastContext = CastContext.getSharedInstance(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        CastButtonFactory.setUpMediaRouteButton(applicationContext, menu, R.id.media_route_menu_item)
+        return true
     }
 }
